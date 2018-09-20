a729499174688766ce421c34dd45eb6c5c4cfdbd
Pacuci <you@example.com>
2018-09-20 09:33:53 +0200

    ExtraerCabeceras

--- Main.py
+++ Main.py
@@ -2,10 +2,8 @@ def readFasta(fasta):
     sequence = ''
     with open(fasta, 'r') as f:
         for line in f:
-            if not line[0] == '>':
+            if line[0] == '>':
                 sequence += line
-            else:
-                sequence += '\n'
     return sequence
 
 sequence = readFasta('pdb_seq.fasta')

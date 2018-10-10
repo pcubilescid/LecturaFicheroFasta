5307d50f7d471fc020596839afdd999e61791097
Pacuci <pcubilescid@gmail.com>
2018-10-10 09:54:16 +0200

    Comment Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
-import sys
+import sys 
 def readFasta(fasta):
     sequence = ''
-    with open(fasta, 'r') as f:
-        for line in f:
-            if line[0] == '>':
-                sequence += line
-    return sequence
+    with open(fasta, 'r') as f: #abrimos fichero fasta, solo lectura
+        for line in f: #para line dentro de fichero
+            if line[0] == '>': #si la 1 posicion de line es >
+                sequence += line <#sequence es igual a line(todos los que empiecen por >)
+    return sequence# retorna sequence
 
 sequence = readFasta(sys.argv[1])
 
